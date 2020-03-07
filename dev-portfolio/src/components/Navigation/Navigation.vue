a47e887e6a8de9d7775be50b8aab3cf53f83729a
lgcavalheiro <lg.cavalheiro@hotmail.com>
2020-03-07 18:12:43 +0000

    more work on navigation, buttons and font awesome

--- dev-portfolio/src/components/Navigation/Navigation.vue
+++ dev-portfolio/src/components/Navigation/Navigation.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
     <header>
-      <h1>{{ head }}</h1>
+      <nav class="navbar navbar-expand-lg justify-content-center">
+        <button type="button" class="btn btn-dark btn-lg mx-5 my-1">About me</button>
+        <button type="button" class="btn btn-dark btn-lg mx-5 my-1">Curriculum</button>
+        <button type="button" class="btn btn-dark btn-lg mx-5 my-1">Portfolio</button>
+        <button type="button" class="btn btn-dark btn-lg mx-5 my-1">Contact</button>
+      </nav>
     </header>
     <footer>
-      <h1>{{ foot }}</h1>
+      <button type="button" class="btn btn-success">Success</button>
     </footer>
   </div>
 </template>
