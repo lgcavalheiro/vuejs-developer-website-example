fa64fc46779477995e171316a58a00c18cfd8cf6
lgcavalheiro <lg.cavalheiro@hotmail.com>
2020-03-07 18:39:53 +0000

    added github and linked in buttons

--- dev-portfolio/src/components/Navigation/Navigation.vue
+++ dev-portfolio/src/components/Navigation/Navigation.vue
@@ -9,7 +9,16 @@
       </nav>
     </header>
     <footer>
-      <button type="button" class="btn btn-success">Success</button>
+      <nav class="navbar navbar-expand-lg justify-content-center">
+        <button type="button" class="btn btn-dark btn-lg mx-5 my-1"
+        onclick=" window.open('https://github.com/lgcavalheiro','_blank')">
+          <i class="fab fa-github"></i>
+        </button>
+        <button type="button" class="btn btn-dark btn-lg mx-5 my-1"
+        onclick=" window.open('https://www.linkedin.com/in/lgcavalheiro/','_blank')">
+          <i class="fab fa-linkedin-in"></i>
+        </button>
+      </nav>
     </footer>
   </div>
 </template>
