7793edd7c657959727ddfe6eb53091d4ff957d8c
lgcavalheiro <lg.cavalheiro@hotmail.com>
2020-03-08 03:54:32 +0000

    started working on portfolio

--- dev-portfolio/src/components/Portfolio/Portfolio.vue
+++ dev-portfolio/src/components/Portfolio/Portfolio.vue
@@ -1,6 +1,38 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
+    <div class="container-fluid" id="containerp">
+      <div class="row">
+        <div class="col-6">
+          <div class="card mb-3">
+            <div class="row no-gutters">
+              <div class="col-4">
+                <img src="../../assets/bg.jpg" class="img-fluid">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-dark btn-sm"
+                  onclick=" window.open('https://github.com/lgcavalheiro/JOBMAA_scrapper','_blank')">
+                    View <i class="fab fa-github"></i>
+                  </button>
+                  <button type="button" class="btn btn-info btn-sm"
+                  onclick=" window.open('https://gitpod.io/#/https://github.com/lgcavalheiro/JOBMAA_scrapper','_blank')">
+                    Launch <i class="fas fa-eye"></i>
+                  </button>
+                </div>
+              </div>
+              <div class="col-8">
+                <div class="card-body">
+                  <h5 class="card-title">JOBMAA</h5>
+                  <p class="card-text">
+                    <strong>Job</strong> <strong>Ma</strong>rket <strong>A</strong>nalyzer. Searches job posting
+                    websites for developer jobs, uses NLP for determining the skills required and then performs analysis
+                    in order to determine the most required developer skills in a given area.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script src="./Portfolio.js"></script>
